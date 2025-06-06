37b63970fd7e9cc678f9a1413d5d3afd9ae10f92
takahashikousuke <241006@std.hi-joho.ac.jp>
2025-06-06 12:22:00 +0900

    chapter8-1

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+//TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
+// ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
+public class Main {
+    public static void main(String[] args) {
+        Hero h = new Hero();
+        h.name = "ミナト";
+        h.hp = 100;
+
+        Matango m1 = new Matango();
+        m1.hp = 50;
+        m1.suffix = 'A';
+
+        Matango m2 = new Matango();
+        m2.hp = 48;
+        m2.suffix = 'B';
+
+        //行動パターン選択
+        h.sit(70);
+        h.slip();
+        h.sit(95);
+        h.run();
+    }
+}
