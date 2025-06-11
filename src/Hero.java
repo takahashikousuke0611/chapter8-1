46fa82bb0887e83879cf714b908494c3bb0fd54f
takahashikousuke <241006@std.hi-joho.ac.jp>
2025-06-11 09:18:33 +0900

    chapter8

--- src/Hero.java
+++ src/Hero.java
@@ -1,25 +1,47 @@
 public class Hero {
     String name;
     int hp;
+    Sword sword;
+
+    public Hero(String name) {
+        this.hp = 100;
+        this.name = name;
+    }
+    public Hero() {
+        this("ダミー");
+    }
+
+    public void attack() {
+        System.out.println(this.name + "は攻撃した！");
+        System.out.println("敵に5ポイントのダメージを与えた！");
+    }
+
     public void Sleep() {
         this.hp = 100;
         System.out.println(this.name + "は、眠って回復した！");
     }
+
     public void sit(int sec) {
         this.hp += sec;
         System.out.println(this.name + "は、" + sec + "秒座った！");
         System.out.println("HPが" + sec + "ポイント回復した");
     }
+
     public void slip() {
         this.hp -= 5;
         System.out.println(this.name + "は、転んだ！");
         System.out.println("5のダメージ！");
     }
+
     public void run() {
         System.out.println(this.name + "は、逃げ出した！");
         System.out.println("GAMEOVER");
         System.out.println("最終HPは" + this.hp + "でした");
     }
+
+    /*public Hero() {
+        this.hp =100;
+    }*/
 }
 
 
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,29 @@
 // ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
 public class Main {
     public static void main(String[] args) {
+        Hero h1 = new Hero("ミナト");
+        System.out.println(h1.hp);
+        System.out.println(h1.name);
+
+        Hero h2 = new Hero();
+        System.out.println(h2.hp);
+        System.out.println(h2.name);
+        /*Wizard w = new Wizard();
+        w.name = "スガワラ";
+        w.hp = 50;
+        w.heal(h1);   //ミナトを回復させる
+        w.heal(h2);   //アサカを回復させる
+        w.heal(h2);   //アサカを回復させる
+        Sword s = new Sword();
+        s.name ="炎の剣";
+        s.damage = 10;
         Hero h = new Hero();
         h.name = "ミナト";
         h.hp = 100;
+        h.sword = s;
+        System.out.println("現在の武器は" + h.sword.name);*/
 
-        Matango m1 = new Matango();
+       /*Matango m1 = new Matango();
         m1.hp = 50;
         m1.suffix = 'A';
 
@@ -18,6 +36,6 @@ public class Main {
         h.sit(70);
         h.slip();
         h.sit(95);
-        h.run();
+        h.run();*/
     }
 }
